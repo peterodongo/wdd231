6c844d55b9aba75cd6aa7f41cdcc52fada44ec9e
Peter Odongo <pochieng1177@gmail.com>
2026-06-15 10:32:49 +0300

    ythr

---
+++ final/scripts/Elern.js
@@ -0,0 +1,28 @@
+// ------------------------------
+// Hamburger Menu
+// ------------------------------
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+
+    // Change icon when menu opens/closes
+    if (menu.classList.contains("show")) {
+        hamburger.textContent = "✖";
+    } else {
+        hamburger.textContent = "☰";
+    }
+});
+
+// ------------------------------
+// Current Year
+// ------------------------------
+const year = document.querySelector("#currentyear");
+year.textContent = new Date().getFullYear();
+
+// ------------------------------
+// Last Modified Date
+// ------------------------------
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
