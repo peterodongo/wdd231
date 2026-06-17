ef3aae370397ade381a8b1539bc639844fda6fee
Peter Odongo <pochieng1177@gmail.com>
2026-06-17 19:39:59 +0300

    0987

--- final/scripts/about.js
+++ final/scripts/about.js
@@ -139,4 +139,16 @@ if (!lastVisit) {
 localStorage.setItem("lastVisit", now);
 
 
+// Current Year
+// ------------------------------
+const year = document.querySelector("#currentyear");
+year.textContent = new Date().getFullYear();
+
+// ------------------------------
+// Last Modified Date
+// ------------------------------
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+
 
